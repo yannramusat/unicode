f04bd3051f714a63d3eb6c5329ed1cd17bcb4242
Yann Ramusat <yann.ramusat@gmail.com>
2024-03-08 21:50:29 +0100

    Add DELETE to CORS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :characters, -> { distinct }, through: :favorites
+
+  validates :username, presence: true
+  validates :mail, presence: true, uniqueness: true
 end
